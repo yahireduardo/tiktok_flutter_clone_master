ebcb18ef351fca17c97447b138ee6b97974ad3dd
yahireduardo <Yahireduardo@hotmail.com>
2025-12-08 15:31:10 -0600

    android y web

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -16,7 +16,7 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-
+ 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.9.1" apply false
